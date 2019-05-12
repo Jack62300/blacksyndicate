950d6b9f71d1f46b9c169cd9d9f388380ece6608
Jack <benoitdelobel@gmail.com>
2019-05-12 14:35:21 +0200

    Add entity service and migrations

---
+++ src/Entity/Service.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ServiceRepository")
+ */
+class Service
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $recrutement;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contenue;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getRecrutement(): ?bool
+    {
+        return $this->recrutement;
+    }
+
+    public function setRecrutement(bool $recrutement): self
+    {
+        $this->recrutement = $recrutement;
+
+        return $this;
+    }
+
+    public function getContenue(): ?string
+    {
+        return $this->contenue;
+    }
+
+    public function setContenue(string $contenue): self
+    {
+        $this->contenue = $contenue;
+
+        return $this;
+    }
+}
